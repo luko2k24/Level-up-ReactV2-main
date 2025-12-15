4bdf8a365b49f875138fbe68d16ca3e94111d73b
luko2k24 <l.meza.cc4@gmail.com>
2025-12-15 16:19:29 -0300

    cambios para el panel admin

---
+++ src/api/service/apiClient.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { API_BASE_URL } from "../config";
+
+export const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("jwt_token");
+
+    if (token) {
+      config.headers = config.headers ?? {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
